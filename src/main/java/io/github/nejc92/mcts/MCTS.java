a41f3a45894f852e7fc5b6c729d3b24290d77582
nejc92 <nejc.il92@gmail.com>
2015-06-10 21:59:00 +0200

    mcts - rename classes conflict

--- src/main/java/io/github/nejc92/mcts/MCTS.java
+++ src/main/java/io/github/nejc92/mcts/MCTS.java
@@ -1,6 +1,6 @@
 package io.github.nejc92.mcts;
 
-public class Mcts<T, S extends MctsDomainState<T>> {
+public class MCTS<T, S extends MctsDomainState<T>> {
     public void test(S state){
         MctsTreeNode<T, S> node = new MctsTreeNode<>(state);
         T action = node.getStatesIncomingAction();
